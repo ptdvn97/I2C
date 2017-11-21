4f58f52fae9e3054c64ea50119c145d50e03c654
ptdvn97 <ptdvn97@gmail.com>
2017-11-22 00:02:37 +0700

    Update

--- slave.c
+++ slave.c
@@ -1,29 +1,43 @@
 #include <slave.h>
+#include <lcd.h>
 
-
-
-
-void main()
-{
-   output_d(0x00);
-   enable_interrupts(INT_SSP);
-   enable_interrupts(GLOBAL);
-   while(TRUE)
-   {}
-
-}
-
+int signal=99,index1=7,index2=1;
 
 #INT_SSP
 void ngatSSP()
 {
-   int a;
-   a = i2c_read();
-   if(a==1)
-   {
-      output_bit(PIN_D0,1);
-      delay_ms(1000);
-      output_bit(PIN_D0,0);
-   }
-   
+   signal = i2c_read();
 }
+
+void showNumb()
+{
+   if(signal!=99)
+   {
+      lcd_gotoxy(index1,index2);
+      printf(lcd_putc,"%d",signal);
+      index1++;
+      if(index1==16)
+      {
+         index2++;
+         index1 = 1;
+      }
+      signal = 99;
+   }
+}
+
+void main()
+{
+   lcd_init();
+   lcd_putc('\f');
+   lcd_gotoxy(1,1);
+   printf(lcd_putc,"Value:");
+   enable_interrupts(INT_SSP);
+   enable_interrupts(GLOBAL);
+   while(1)
+   {
+      showNumb();
+   }
+}
+
+
+
